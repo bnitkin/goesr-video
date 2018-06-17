e15cb3ee13703d428a67d81cd94862a1c2fd5a6a
Ben Nitkin <ben@nitkin.net>
2018-06-17 12:32:07 -0600

    Improved status.sh monitor; center video vertically in webpage; fix crontab's redirect

--- status.sh
+++ status.sh
@@ -1,3 +1,8 @@
 echo 'videogen  ' `pidof -x videogen.py`
 echo 'compositor' `pidof -x GOES16.sci.py`
 echo 'server    ' `pidof -x goesServer.py`
+echo
+echo
+ps -p "`pidof -x GOES16.sci.py nccopy videogen.py ffmpeg goesServer.py`" -o %cpu,%mem,cmd
+echo
+free -h
