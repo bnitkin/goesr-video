3372df06aad39489ae805701c791ac1565db6777
Ben Nitkin <ben@nitkin.net>
2018-06-20 21:47:07 -0600

    Status.sh now shows pids and statuses of each job

--- status.sh
+++ status.sh
@@ -1,8 +1,16 @@
-echo 'videogen  ' `pidof -x videogen.py`
-echo 'compositor' `pidof -x GOES16.sci.py`
-echo 'server    ' `pidof -x goesServer.py`
-echo
-echo
-ps -p "`pidof -x GOES16.sci.py nccopy videogen.py ffmpeg goesServer.py`" -o %cpu,%mem,cmd
-echo
-free -h
+#!/bin/bash
+[ 'a'$1 == 'awatch' ] && (watch -n15 'bash -c "~bnitkin/status.sh | cut -c -$COLUMNS"'; exit)
+
+# PID of each process and the last line of each logfile
+echo -e "videogen:  $(pidof -x videogen.py || echo -e \\t) \t| $(echo /tmp/*webm)"
+echo -e "compositor:$(pidof -x GOES16.sci.py             ) \t| $(tac ~bnitkin/log.txt | grep -m1 '^Total')"
+echo -e "server:    $(pidof -x goesServer.py             ) \t| $(tail -n1 ~bnitkin/server.txt)"
+echo ''
+# List of processes that these guys spawn
+echo '%CPU %MEM CMD'
+ps -p "`pidof -x GOES16.sci.py nccopy videogen.py ffmpeg goesServer.py`" -o %cpu=,%mem=,cmd= | sort -k3 -t' '
+echo ''
+echo ''
+free -t --mega
+echo ''
+df   -h ~bnitkin
