2ede895637a8b92247539a2b266186b106237288
Ben Nitkin <ben@nitkin.net>
2018-06-24 10:49:54 -0600

    Intelligently find noon for each day instead of naively going back 96 images at a time.

--- videogen.py
+++ videogen.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # Simple script to turn a bunch of photos into a video
 import os, sys, glob, subprocess
+from datetime import datetime
+
+#sys.argv[2]
 
 FRAMES = 15*24*2 # 2 days of data
 SRC =  '/home/bnitkin/goesr/truecolor-thumb-*.jpg'
@@ -8,8 +11,8 @@ DEST = '/home/bnitkin/goesr/video-{}.webm'
 FRAMES = {'day':     24*4-1,
           'two-day': 24*4*2-1,
           'week':    24*4*7-1,
-          'month':   24*4*31-1,
-          'year':    24*4*365-1}
+          'month':   31,
+          'year':    365}
 
 # frames per second (real time is four frames per hour)
 RATE   = {'day':     '8',
@@ -20,20 +23,26 @@ RATE   = {'day':     '8',
 
 
 def main():
-    files = sorted(glob.glob(SRC))[-FRAMES[sys.argv[1]]:]
-    print('Encoding the following', len(files), 'files:', files)
+    files = sorted(glob.glob(SRC))
 
     # for month and year settings, use daily pictures instead of 15-minutes.
-    # This iterates backwards, inserting every 24*4 images from the most recent.
-    files_decimate = []
+    # This uses some cleverness to find the file closest to noon for each day.
     if sys.argv[1] in ['month', 'year']:
-        for index in range(len(files)):
-             if index%(24*4) == 0:
-                 print(len(files) - index - 1)
-                 print(files[len(files) - index - 1])
-                 files_decimate.insert(0, files[len(files) - index - 1])
-        print('Decimated to', len(files_decimate), 'files:', files_decimate)
-        files = files_decimate
+        # Build a sorted list of file mtime and path
+        files = [(os.path.getmtime(f), f) for f in files]
+        files_daily = []
+        noon = datetime.utcnow().replace(hour=16, minute=40, second=0).timestamp()
+        for index in range(FRAMES[sys.argv[1]]):
+            ideal_time = noon - 3600*24*index
+            file       = min(files, key=lambda f: abs(f[0] - ideal_time))[1]
+            # Insist that "noon" imagery be +/- an hour of noon.
+            if abs(os.path.getmtime(file) - ideal_time) > 3600: continue
+            files_daily.insert(0, file)
+        files = files_daily
+    else:
+        files = files[-FRAMES[sys.argv[1]]:]
+
+    print('Encoding the following', len(files), 'files:', files)
 
     tmpfile =  '/tmp/video-{}.webm'.format(sys.argv[1])
     if os.path.isfile(tmpfile):
@@ -46,9 +55,19 @@ def main():
         '-y',                          # Overwrite output path
         '-f', 'image2pipe', '-i', '-', # Read images from stdin
         '-pix_fmt', 'yuv420p',         # Force older pixel format to make Firefox happy.
-        '-c:v', 'libvpx-vp9',
-        '-crf', '30', '-b:v', '0',     # Constant quality (31 is suggested for 1080p)
-        tmpfile), stdin=subprocess.PIPE)  # Write to a tempfile
+        '-c:v', 'libvpx-vp9', '-an',
+# Settings to make it go a litte faster, but at a quality tradeoff.
+#        '-cpu-used', '2',
+#        '-speed', '3',
+        '-crf', '31', '-b:v', '0',     # Constant quality (31 is suggested for 1080p)
+        tmpfile,
+# Uncomment the below to add a mp4 output. Should work on Safari, but doesn't...
+#        '-c:v', 'h264', '-c:a', 'aac',
+#        '-pix_fmt', 'yuv420p',
+#        '-preset', 'veryfast',
+#        '-crf', '30', '-b:v', '0',     # Constant quality (18-24 is suggested for mp4)
+#        tmpfile.replace('webm', 'mp4')
+        ), stdin=subprocess.PIPE)      # Write to a tempfile
 
     for path in files:
         with open(path, 'rb') as image:
@@ -59,5 +78,6 @@ def main():
     # Finally, move to the output area. 
     # This ensures that the final move is atomic, and reduces web-frontend screwups.
     os.rename('/tmp/video-{}.webm'.format(sys.argv[1]), DEST.format(sys.argv[1]))
+#    os.rename('/tmp/video-{}.mp4'.format(sys.argv[1]), DEST.format(sys.argv[1]).replace('webm', 'mp4'))
 
 main()
